5602b47f1e6812451c395a7b80160e77cdb81eb1
thismkhy <mkhy88519@gmail.com>
2022-05-27 16:19:26 +0200

    config firebase and connect to the app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import { AuthContextProvider } from './contextApi/AuthContext';
+import Account from "./pages/Account";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <>
+    <AuthContextProvider>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
 
       </Routes>
+    </AuthContextProvider>
     </>
   );
 }
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+const Signup = () => {
+  return (
+    <div>Signup</div>
+  );
+};
+
+export default Signup;
